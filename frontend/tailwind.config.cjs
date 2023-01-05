b4f85d42160bc726cebc1496266c51d986274919
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-01-05 16:15:08 +0100

    tailwind config

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  colors: {
+    // primary and secondary are the dominant colors
+    primary: "#BD3944", // red (dark)
+    secondary: "#042E27", // green (dark)
+    tertiary: "#364966", // blue (dark)
+    // the rest follow text color when dark mode is enabled you can change the colors to match your theme
+    quaternary: "#111111", // black (dark) -> for text
+    quinary: "white", // white -> for text
+    senary: "#232122", // bg color (dark)
+    gray: "#F4F4F4"
+  },
+  plugins: [],
+}
