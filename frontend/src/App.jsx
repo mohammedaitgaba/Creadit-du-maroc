1862fb3f03c002ce118628739715f117e42c62f5
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-01-13 16:09:23 +0100

    home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import PhotoInput from './componnents/uploader'
+import Home from './pages/Home';
 import NavBar from './componnents/Navbar'
+import Profile from './pages/Profile';
+import { Footer } from './componnents/Footer';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
@@ -9,7 +12,10 @@ const App = () => {
                 <NavBar />
                 <Routes>
                   <Route path='/up' element={<PhotoInput/>}/>
+                  <Route path='/profile' element={<Profile/>}/>
+                  <Route path='/' element={<Home/>}/>
                 </Routes>
+                <Footer />
             </BrowserRouter>
     </div>
   )
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Banner from '../componnents/homeComponnents/Banner'
+import { Content } from '../componnents/homeComponnents/Content'
+import { Clients } from '../componnents/homeComponnents/Clients'
+const Home = () => {
+  return (
+    <div>
+      <Banner/>
+      <Content/>
+      <Clients/>
+    </div>
+  )
+}
+
+export default Home
