43eeba4db7f8906f3d81d02634222263f179efcd
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-01-14 17:27:04 +0100

    add styles to Home banner

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -12,6 +12,7 @@ module.exports = {
     primary: "#BD3944", // red (dark)
     secondary: "#042E27", // green (dark)
     tertiary: "#364966", // blue (dark)
+    main:"#0a72d1",
     // the rest follow text color when dark mode is enabled you can change the colors to match your theme
     quaternary: "#111111", // black (dark) -> for text
     quinary: "white", // white -> for text
