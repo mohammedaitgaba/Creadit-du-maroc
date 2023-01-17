c1343acdfbe473483d9f0476825c169d4331915e
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-01-17 16:28:11 +0100

    Pretected routes in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home';
 import NavBar from './componnents/Navbar';
+import AdminAuth from './pages/AdminAuth';
 import UserDushboard from './pages/UserDushboard';
 import { Footer } from './componnents/Footer';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminDushboard from './pages/AdminDushboard';
-import { ToastContainer } from 'react-toastify';
-
-import 'react-toastify/dist/ReactToastify.css';
-
+import ProtectedRoutes from './utils/ProtectedRoutes';
+import OnlyAdminRouts from './utils/OnlyAdminRouts';
 const App = () => {
   return (
     <div>
-            <BrowserRouter>
-                <NavBar />
-                <Routes>
-                  <Route path='/UserDushboard/*' element={<UserDushboard/>}/>
-                  <Route path='/' element={<Home/>}/>
-                  <Route path='/Admin' element={<AdminDushboard/>}/>
-                </Routes>
-                <Footer />
-                <ToastContainer/>
-            </BrowserRouter>
+      <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={[<Home/>, <Footer/>]}/>
+            <Route path='/AdminLogin' element={<AdminAuth/>}/>
+            <Route element={<ProtectedRoutes/>}>
+              <Route path='/UserDushboard/*' element={<UserDushboard/>}/>
+            </Route>            
+            <Route element={<OnlyAdminRouts/>}>
+              <Route path='/Admin' element={<AdminDushboard/>}/>
+            </Route>
+          </Routes>
+          <ToastContainer/>
+      </BrowserRouter>
     </div>
   )
 }
